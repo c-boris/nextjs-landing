b1d7533574cdfb28224bc7d4e4f889277cb0b862
cboris <b.crochet@gmail.com>
2024-09-17 11:44:55 +0200

    up

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,24 +1,33 @@
-import NewsLatterBox from "./NewsLatterBox";
+// import NewsLatterBox from "./NewsLatterBox";
+import SectionTitle from "../Common/SectionTitle";
 
 const Contact = () => {
   return (
     <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28">
       <div className="container">
-        <div className="-mx-4 flex flex-wrap">
-          <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
+        <SectionTitle
+          title="Nous contacter"
+          paragraph="Pour toute correspondance, veuillez utiliser le formulaire
+                ci-dessous. Nous sommes à votre disposition pour répondre à vos
+                questions et vous assister."
+          center
+          width="665px"
+        />
+        <div className="-mx-4 flex flex-wrap ">
+          <div className="mx-auto w-full px-4 lg:w-7/12 xl:w-8/12">
             <div
               className="mb-12 rounded-md bg-indigo-100 px-8 py-11 shadow-three dark:bg-gray-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
               data-wow-delay=".15s
               "
             >
-              <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
+              {/* <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
                 Nous contacter
-              </h2>
-              <p className="mb-12 text-base font-medium text-body-color">
+              </h2> */}
+              {/* <p className="mb-12 text-base font-medium text-body-color">
                 Pour toute correspondance, veuillez utiliser le formulaire
                 ci-dessous. Nous sommes à votre disposition pour répondre à vos
                 questions et vous assister.
-              </p>
+              </p> */}
               <form>
                 <div className="-mx-4 flex flex-wrap">
                   <div className="w-full px-4 md:w-1/2">
