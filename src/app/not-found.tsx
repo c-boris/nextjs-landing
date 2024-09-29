170f2de8327e0527f815d477fa5422f23de1548d
cboris <b.crochet@gmail.com>
2024-09-29 17:13:07 +0200

    up

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,9 +2,8 @@ import Link from "next/link";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Error Page | Free Next.js Template for Startup and SaaS",
-  description: "This is Error Page for Startup Nextjs Template",
-  // other metadata
+  title: "Studio 126 | Not found",
+  description: "Désolé, la page est introuvable.",
 };
 
 const ErrorPage = () => {
