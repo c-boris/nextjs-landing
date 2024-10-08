86f8df84fd692356db9d398a46bbaf0d5c0a9c7e
cboris <b.crochet@gmail.com>
2024-10-08 17:42:15 +0200

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,17 @@ import Script from "next/script";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
-import { GeistSans } from "geist/font/sans";
+// import { GeistSans } from "geist/font/sans";
 import "../styles/index.css";
 import { Toaster } from "react-hot-toast";
 import CookieConsent from "@/components/CookieConsent";
 import { Providers } from "./providers";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+});
 
 export default function RootLayout({
   children,
@@ -17,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr" suppressHydrationWarning>
-      <body className={`bg-[#FCFCFC] dark:bg-black ${GeistSans.className}`}>
+      <body className={`bg-[#FCFCFC] dark:bg-black ${poppins.className}`}>
         <Providers>
           <Header />
           {children}
